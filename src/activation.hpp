11023d2f4307bb1832ec37aaf753624c4506e079
Abidan Brito <43681148+abidanBrito@users.noreply.github.com>
2026-01-01 21:19:21 +0000

    feat(activation): add sigmoid function

--- src/activation.hpp
+++ src/activation.hpp
@@ -5,6 +5,7 @@
 namespace ann::activation
 {
 
+    // Identity function
     [[nodiscard]] constexpr auto linear(double x) -> double
     {
         return x;
@@ -13,6 +14,17 @@ namespace ann::activation
     [[nodiscard]] constexpr auto linear_derivative([[maybe_unused]] double x) -> double
     {
         return 1.0;
+    }
+
+    // Logistic function (sigmoid curve)
+    [[nodiscard]] constexpr auto sigmoid(double x) -> double
+    {
+        return 1.0 / (1.0 + std::exp(-x));
+    }
+
+    [[nodiscard]] constexpr auto sigmoid_derivative(double output) -> double
+    {
+        return output * (1.0 - output);
     }
 
 } // namespace ann::activation
